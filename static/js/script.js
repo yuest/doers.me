3f823afa1b4702647ad6a3625e1ed1cfc66b8c9d
宇蠢Yuest <yuestwang@gmail.com>
2011-10-13 20:48:10 +0800

    start using backbone for List

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
 jQuery( function ( $ ) {
-    window.Task = Backbone.Model.extend({
+    window.MList = Backbone.Model.extend({
+        defaults: {
+            title: ''
+        }
     });
-    $('.project:eq(0)').clone().addClass('focused').appendTo('body');
+
+    window.VList = Backbone.View.extend({
+        tagName: 'section'
+        ,className: 'project focused'
+        ,template: doT.template(' <div class="move-handler jMoveHandler"></div> <h1 class="jProjectName" contenteditable>{{=it.title}}</h1> <ul></ul> ')
+        ,initialize: function () {
+            $( this.el ).html( this.template( this.model.toJSON())).appendTo('body');
+            this.$('.jProjectName').trigger('focus');
+        }
+    });
+
+    new VList({model: new MList()});
+    //$('.project:eq(0)').clone().addClass('focused').appendTo('body');
     $('.project:eq(1)').css({ left: '700px' })
     .on('focus', '.jProjectName', function ( ev ) {
         var $el = $( this );
@@ -17,8 +32,12 @@ jQuery( function ( $ ) {
         console.log( $el );
     })
     .on('keydown', 'span[contenteditable]', function ( ev ) {
-        if (ev.keyCode == 8 && !$(this).text().length) {
+        var $el = $( this );
+        if (ev.keyCode == 8 && !$el.text().length && $el.height() <= 24) {
             return false;
-        };
+        }
+        if (ev.keyCode == 27) {
+            $el.trigger('blur');
+        }
     });
 });
